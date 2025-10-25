c1ad89dc1f31af7ff5eec4f50853f9b39eb85a2a
Ethelbert-cell <chimelueethelbert@gmail.com>
2025-10-25 11:26:58 +0100

    adding the sorting logic to app.js

--- app.js
+++ app.js
@@ -1,87 +1,102 @@
 new Vue({
-    el: '#app',
-    data: {
-        lessons: [
-            {
-                id: 1,
-                subject: 'Mathematics',
-                location: 'London',
-                price: 100,
-                spaces: 5,
-                image: 'images/math.png'
-            },
-            {
-                id: 2,
-                subject: 'English',
-                location: 'London',
-                price: 90,
-                spaces: 5,
-                image: 'images/english.png'
-            },
-            {
-                id: 3,
-                subject: 'Science',
-                location: 'Manchester',
-                price: 110,
-                spaces: 5,
-                image: 'images/science.png'
-            },
-            {
-                id: 4,
-                subject: 'History',
-                location: 'Bristol',
-                price: 80,
-                spaces: 5,
-                image: 'images/history.png'
-            },
-            {
-                id: 5,
-                subject: 'Art',
-                location: 'London',
-                price: 120,
-                spaces: 5,
-                image: 'images/art.png'
-            },
-            {
-                id: 6,
-                subject: 'Music',
-                location: 'Manchester',
-                price: 95,
-                spaces: 5,
-                image: 'images/music.png'
-            },
-            {
-                id: 7,
-                subject: 'Geography',
-                location: 'Bristol',
-                price: 85,
-                spaces: 5,
-                image: 'images/geography.png'
-            },
-            {
-                id: 8,
-                subject: 'Physical Education',
-                location: 'London',
-                price: 75,
-                spaces: 5,
-                image: 'images/pe.png'
-            },
-            {
-                id: 9,
-                subject: 'Computer Science',
-                location: 'Manchester',
-                price: 130,
-                spaces: 5,
-                image: 'images/cs.png'
-            },
-            {
-                id: 10,
-                subject: 'Drama',
-                location: 'Bristol',
-                price: 105,
-                spaces: 5,
-                image: 'images/drama.png'
-            }
-        ]
-    }
+  el: "#app",
+  data: {
+    lessons: [
+      {
+        id: 1,
+        subject: "Mathematics",
+        location: "London",
+        price: 100,
+        spaces: 5,
+        image: "images/math.png",
+      },
+      {
+        id: 2,
+        subject: "English",
+        location: "London",
+        price: 90,
+        spaces: 5,
+        image: "images/english.png",
+      },
+      {
+        id: 3,
+        subject: "Science",
+        location: "Manchester",
+        price: 110,
+        spaces: 5,
+        image: "images/science.png",
+      },
+      {
+        id: 4,
+        subject: "History",
+        location: "Bristol",
+        price: 80,
+        spaces: 5,
+        image: "images/history.png",
+      },
+      {
+        id: 5,
+        subject: "Art",
+        location: "London",
+        price: 120,
+        spaces: 5,
+        image: "images/art.png",
+      },
+      {
+        id: 6,
+        subject: "Music",
+        location: "Manchester",
+        price: 95,
+        spaces: 5,
+        image: "images/music.png",
+      },
+      {
+        id: 7,
+        subject: "Geography",
+        location: "Bristol",
+        price: 85,
+        spaces: 5,
+        image: "images/geography.png",
+      },
+      {
+        id: 8,
+        subject: "Physical Education",
+        location: "London",
+        price: 75,
+        spaces: 5,
+        image: "images/pe.png",
+      },
+      {
+        id: 9,
+        subject: "Computer Science",
+        location: "Manchester",
+        price: 130,
+        spaces: 5,
+        image: "images/cs.png",
+      },
+      {
+        id: 10,
+        subject: "Drama",
+        location: "Bristol",
+        price: 105,
+        spaces: 5,
+        image: "images/drama.png",
+      },
+    ],
+    sortAttribute: "subject",
+    sortOrder: "asc",
+  },
+  computed: {
+    sortedLessons() {
+      return this.lessons.sort((a, b) => {
+        let comparison = 0;
+        if (a[this.sortAttribute] > b[this.sortAttribute]) {
+          comparison = 1;
+        } else if (a[this.sortAttribute] < b[this.sortAttribute]) {
+          comparison = -1;
+        }
+        return this.sortOrder === "asc" ? comparison : -comparison;
+      });
+    },
+  },
 });
